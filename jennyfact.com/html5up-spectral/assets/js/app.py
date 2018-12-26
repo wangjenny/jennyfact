f830999184ebad1e1333d72741b50daee0990529
Jenny Wang <5914322+wangjenny@users.noreply.github.com>
2018-12-25 23:05:40 -0500

    Local to github
    Local repository to use github version control

---
+++ jennyfact.com/html5up-spectral/assets/js/app.py
@@ -0,0 +1,11 @@
+from random import *
+import csv
+
+def get_fact():
+    with open('jenny_fun_facts.txt') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count
+    numLines =
+    randomInt =
